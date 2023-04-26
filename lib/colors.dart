fcdbf6ffd34be002288eb8bc91b3d5ea2cd21053
Pedro Trama <80865643+PedroTrama@users.noreply.github.com>
2023-04-26 15:46:37 -0300

    Update colors.dart

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,7 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:ping/settings.dart';
 
 class ColorsScreen extends StatelessWidget {
-  const ColorsScreen({Key? key}) : super(key: key);
+  ColorsScreen({Key? key}) : super(key: key);
+
+  final List text = ["Branco", "Vermelho", "Azul", "Amarelo", "Verde"];
+
+  final List colors = [
+    Colors.white,
+    Color(0xffd23569),
+    Color(0xff389ae8),
+    Color(0xffecdd5a),
+    Color(0xff69dc6d)
+  ];
+
+  final List icons = [
+    Icons.brush,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,35 +30,35 @@ class ColorsScreen extends StatelessWidget {
       body: Row(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(
-                    'Cor $index',
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
-                    textAlign: TextAlign.center,
-                  ),
-                );
-              },
-            ),
-          ),
+              child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                color: colors[index % colors.length],
+                child: ListTile(
+                    leading: Icon(Icons.brush),
+                    title: Text(text[index % text.length])),
+              );
+            },
+          )),
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(
-                    'Cor $index',
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
-                    textAlign: TextAlign.center,
-                  ),
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  color: colors[index % colors.length],
+                  child: ListTile(
+                      leading: Icon(Icons.brush),
+                      title: Text(text[index % text.length])),
                 );
               },
             ),
-          ),
+          )
         ],
       ),
     );
   }
 }
+
