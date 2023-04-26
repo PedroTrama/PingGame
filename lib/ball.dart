d929492bb47625f2d7f947eebf933c44c1c5e951
Pedro Trama <80865643+PedroTrama@users.noreply.github.com>
2023-04-26 16:35:07 -0300

    Update ball.dart

--- lib/ball.dart
+++ lib/ball.dart
@@ -12,13 +12,10 @@ class Ball extends StatelessWidget {
     return Container(
       alignment: Alignment(x.toDouble(), y.toDouble()),
       child: Container(
-          decoration:
-              const BoxDecoration(
-                shape: BoxShape.circle, 
-                color: Colors.white
-              ),
-              width: 20,
-              height: 20,
+        decoration:
+            const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+        width: 20,
+        height: 20,
       ),
     );
   }
