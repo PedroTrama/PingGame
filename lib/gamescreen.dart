ea0411733185e01b41a8aabbfbcb0a1bd8cbf37f
Pedro Trama <80865643+PedroTrama@users.noreply.github.com>
2023-04-26 16:36:47 -0300

    Update gamescreen.dart

--- lib/gamescreen.dart
+++ lib/gamescreen.dart
@@ -1,13 +1,14 @@
-//This class is the game's screen. It contains every element of the game
+//This is the main screen of the game, where the players control the ball to hit the bars
 import 'package:flutter/material.dart';
 import 'bars.dart';
 import 'ball.dart';
+import 'mainmenu.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       body: Center(
@@ -21,10 +22,43 @@ class GameScreen extends StatelessWidget {
 
             //ball
             Ball(x: 0, y: 0),
-            
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Tem certeza que deseja sair do jogo?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MenuScreen()),
+                    );
+                  },
+                  child:
+                      const Text('Sim', style: TextStyle(color: Colors.black)),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child:
+                      const Text('Não', style: TextStyle(color: Colors.black)),
+                ),
+              ],
+            ),
+          );
+        },
+        backgroundColor: Colors.grey[900],
+        elevation: 0,
+        child: const Icon(Icons.exit_to_app),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
