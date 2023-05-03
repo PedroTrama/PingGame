99c075b4444044309897687d8df31e15a2101a5c
Pedro Trama <80865643+PedroTrama@users.noreply.github.com>
2023-05-03 17:00:59 -0300

    Update bars.dart

--- lib/bars.dart
+++ lib/bars.dart
@@ -1,17 +1,22 @@
 //This class controls the bars and sets a few defaults
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-class Bars extends StatelessWidget {
+class Bars extends StatefulWidget {
   //Coordinates
   final x;
   final y;
+  const Bars({Key? key, this.x, this.y}) : super(key: key);
 
-  const Bars({super.key, this.x, this.y});
+  @override
+  BarsState createState() => BarsState();
+}
 
+class BarsState extends State<Bars> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment(x.toDouble(), y.toDouble()),
+      alignment: Alignment(widget.x.toDouble(), widget.y.toDouble()),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Container(
